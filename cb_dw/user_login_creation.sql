b295d803e93069ab8a4a40b1a71e296da19e7942
Phil Steffek <psteffek@cloudbees.com>
2019-01-07 17:28:07 -0800

    renamed table to user_login_summary from user_login_creation.

--- cb_dw/user_login_creation.sql
+++ cb_dw/user_login_creation.sql
@@ -2,15 +2,19 @@
 -- Drop table
  drop
 	table
-		if exists user_login_creation;
+		if exists user_login_summary;
 
 create
 	table
-		user_login_creation (
-			dw_user_event_id bigserial not null primary key
+		user_login_summary (
+			dw_user_login_summary_id bigserial not null primary key
 			, dw_user_id bigserial not null
 			, login_type character varying (100) not null -- e.g. codeship or grandcentral
-			, login_created timestamp not null
-			, login_deleted timestamp null
-			, constraint uk_user_login_creation unique (dw_user_id, login_type, login_created)
+			, login_created_at timestamp not null
+			, login_deleted_at timestamp null
+			, dw_created_at timestamp not null
+			, dw_updated_at timestamp not null
+			, dw_created_by character varying not null
+			, dw_updated_by character varying not null
+			, constraint uk_user_login_summary_dw_user_id_login_type_login_created_at unique (dw_user_id, login_type, login_created_at)
 		);
